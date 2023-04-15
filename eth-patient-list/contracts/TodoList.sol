79d683096681e4d0cedf1554af26c847ebc3cf96
SubhavBatra <subhavbatra3002@gmail.com>
2023-04-16 02:22:06 +0530

    eth patient

---
+++ eth-patient-list/contracts/TodoList.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
+    constructor() public {
+        createTask("506,50,M,0,0,1,1,0,13.1,10.5,288,116,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+  }
+}
